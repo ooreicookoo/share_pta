1da3837c7d59087bd868a0b38badd85cc883f4f9
reiko enomoto <ooreicookoo@gmail.com>
2021-01-19 22:52:38 +0900

    create migration file of reports

---
+++ db/migrate/20210119122442_create_reports.rb
@@ -0,0 +1,12 @@
+class CreateReports < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reports do |t|
+      t.string :title, null: false
+      t.text :content
+      t.integer :time, null: false
+      t.date :date, null: false
+      t.string :place
+      t.text :image
+    end
+  end
+end
