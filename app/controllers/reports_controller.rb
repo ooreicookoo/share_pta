4640875b88d518f010f5c8c5ca026c30ca6d9ff0
reiko enomoto <ooreicookoo@gmail.com>
2021-01-20 16:22:43 +0900

    レポート詳細画面まで実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,19 @@
 class ReportsController < ApplicationController
   def index
+    @reports = Report.all
   end
   def new
+    @report = Report.new
+  end
+  def create
+    Report.create(report_params)
+    redirect_to new_report_path
+  end
+  def show
+    @report = Report.find(params[:id])
+  end
+  private
+  def report_params
+    params.require(:report).permit(:title, :content, :time, :date, :place, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'reports/index'
   resources :reports
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
