63921eecf5f0e910e2a9de92ea02df6e70a6381e
reiko enomoto <ooreicookoo@gmail.com>
2021-01-21 16:41:46 +0900

    deviseのsign_upの画面まで実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :reports do
     collection do
       post :confirm
     end
+  end
+  root 'reports#index'
+  
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 end
