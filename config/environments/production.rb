0a3608d989786710b9d82affb450cb594c13ed68
reiko enomoto <ooreicookoo@gmail.com>
2021-01-22 23:31:46 +0900

    re:deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'share-task-pta.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'share-pta.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
