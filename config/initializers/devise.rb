4811613ffcc17b415af5f9b60ecd66b74fb9c410
reiko enomoto <ooreicookoo@gmail.com>
2021-01-27 18:36:46 +0900

    devise.rb にENV['SENDGRID_API_KEY']を記述

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,9 +25,10 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender =  ENV['SENDGRID_API_KEY']
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
                password_confirmation: password,
                )
 end
+
+# 20.times do |k|
+#   Report.create!(
+#   title:"report#{k + 1}",
+#   content: "content#{k + 1}",
+#   content: "content#{k + 1}",
+#   time: '2.5',
+#   date: '2020-09-14 00:00:00',
+#   place: '学校',
+#   user_id: User.first.id + k
+#   )
+# end
