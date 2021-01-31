7f93ada4f928c8eb53a4fa14dfea0f3dc88bc739
reiko enomoto <ooreicookoo@gmail.com>
2021-01-31 18:11:11 +0900

    team編集画面まで取り掛かり中

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,18 +1,18 @@
 class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
   def index
     @teams = Team.all
   end
 
   def new
     @team = Team.new
-    @assign << current_user
   end
 
   def create
     @team = Team.new(team_params)
-    @team.owner = current_user
+    # @team.owner = current_user
     if @team.save
-      redirect_to  root_path, notice: 'チームを作成しました！'
+      redirect_to  teams_path, notice: 'チームを作成しました！'
     else
       render :new
     end
@@ -24,12 +24,23 @@ class TeamsController < ApplicationController
   def edit
   end
 
+  def update
+    if @team.update(team_params)
+      redirect_to teams_path, notice: "チーム名を編集しました！"
+    else
+      render :edit
+    end
+  end
+
   def invite
     #サービスを利用していない人をに招待する機能
   end
 
   private
   def team_params
-    params.require(:team). permit(:name, user_id: [])
+    params.require(:team). permit(:name)
+  end
+  def set_team
+    @team = Team.find(params[:id])
   end
 end
--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,7 @@
 module TeamsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create' || 'edit'
+      team_path
+    end
+  end
 end
