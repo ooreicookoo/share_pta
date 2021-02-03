7cb9d15198a48d7d1c2c67cdf22d265dc5cb978e
reiko enomoto <ooreicookoo@gmail.com>
2021-02-03 16:03:13 +0900

    コメントの編集機能実装

--- app/controllers/report_comments_controller.rb
+++ app/controllers/report_comments_controller.rb
@@ -1,19 +1,43 @@
 class ReportCommentsController < ApplicationController
+  before_action :set_report, only: [:create, :edit, :update]
   def create
-      @report = Report.find(params[:report_id])
       @report_comment =  @report.report_comments.build(report_comment_params)
       respond_to do |format|
-        if @report_comment.save!
-          format.js { render :index }
-        else
-          format.html { redirect_to report_path(@report), notice: '投稿できませんでした...' }
-        end
+      if @report_comment.save!
+        format.js { render :index }
+      else
+        format.html { redirect_to report_path(@report), notice: '投稿できませんでした...' }
       end
     end
+  end
 
-    private
-
-    def report_comment_params
-      params.require(:report_comment).permit(:report_id, :comment_content)
+  def edit
+    @report_comment = @report.report_comments.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
     end
   end
+
+  def update
+    @report_comment = @report.report_comments.find(params[:id])
+      respond_to do |format|
+        if @report_comment.update(report_comment_params)
+          flash.now[:notice] = 'コメントが編集されました'
+          format.js { render :index }
+        else
+          flash.now[:notice] = 'コメントの編集に失敗しました'
+          format.js { render :edit_error }
+        end
+      end
+  end
+
+  private
+  def report_comment_params
+    params.require(:report_comment).permit(:report_id, :comment_content)
+  end
+
+  def set_report
+    @report = Report.find(params[:report_id])
+  end
+end
