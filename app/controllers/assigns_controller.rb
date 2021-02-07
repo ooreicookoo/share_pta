636b6cc09cffc175d2918f23a4445fe80943caa4
reiko enomoto <ooreicookoo@gmail.com>
2021-02-07 21:39:45 +0900

    チームに登録する実装

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -1,27 +1,31 @@
 class AssignsController < ApplicationController
 
   def new
+    @team = Team.find(params[:team_id])
     @assign = Assign.new
   end
 
   def create
     @assign = current_user.assigns.build(assign_params)
-    team = find_team(params[:team_id])
-    user = current_user
-      if @assign.save
-        redirect_to team_path(team.id), notice: 'チームに参加しました'
-      else
-        render :new
-      end
+    if @assign.save!
+      redirect_to team_path(@assign.team.id), notice: 'チームに参加しました'
+    else
+      render :new
     end
-
+  end
 
   def show
   end
 
+  # ISSUE: what is permit
   private
   def assign_params
-    params.require(:assign).permit(:emai, :team_id, :user_id)
+    params.permit(:id, :team_id, :user_id)
   end
-
 end
+
+
+# ISSUE: don't understand form_with
+# WHAT:
+# WHY: 
+# HOW:
