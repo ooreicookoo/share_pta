6ba2297b677839f1a2da0c19b944016a888ac560
reiko enomoto <ooreicookoo@gmail.com>
2021-02-07 13:59:44 +0900

    fix:チーム名登録のバリデーション

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,12 +1,11 @@
 class Team < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, uniqueness: true
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   has_many :users, through: :assigns
   has_many :assigns, dependent: :destroy
   has_many :reports, dependent: :destroy
 
   with_options on: :invite? do
-    validates :name,  presence: true
+    validates :email,  presence: true
   end
 end
